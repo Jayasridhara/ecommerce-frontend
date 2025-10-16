0abee53fb5255dd6ba61d59b00991a4aaaf2f432
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-10-16 15:40:58 +0530

    push navbar change,orderdetails shown

--- src/instance/protectedInstance.js
+++ src/instance/protectedInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000/api/v1";
-//  const baseURL = "https://ecommerce-backend-1-ldht.onrender.com/api/v1";
+// const baseURL = "http://localhost:5000/api/v1";
+ const baseURL = "https://ecommerce-backend-1-ldht.onrender.com/api/v1";
 
 const protectedInstance = axios.create({
   baseURL,
