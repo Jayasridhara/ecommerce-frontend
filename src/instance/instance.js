e9ac7070c47a83cba0788888ff6ac92e11c7e537
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-10-23 00:27:58 +0530

    push

--- src/instance/instance.js
+++ src/instance/instance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // const baseURL = "http://localhost:5000/api/v1";
 const baseURL = "https://ecommerce-backend-1-ldht.onrender.com/api/v1";
-
+//changes
 const instance = axios.create({
     baseURL,
     timeout: 10000,
--- src/instance/protectedInstance.js
+++ src/instance/protectedInstance.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 // const baseURL = "http://localhost:5000/api/v1";
 const baseURL = "https://ecommerce-backend-1-ldht.onrender.com/api/v1";
-
+//changes
 const protectedInstance = axios.create({
   baseURL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
+
   },
   withCredentials: true, // use cookies (httpOnly token set by backend)
 });
