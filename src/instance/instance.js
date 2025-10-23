d965e388726eb2ded3b39f610606b2bdca11aba4
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-10-23 17:00:28 +0530

    push

--- src/instance/instance.js
+++ src/instance/instance.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 
-const baseURL = "http://localhost:5000/api/v1";
-// const baseURL = "https://ecommerce-backend-1-ldht.onrender.com/api/v1";
-//changes
+// const baseURL = "http://localhost:5000/api/v1";
+const baseURL = "https://ecommerce-backend-1-ldht.onrender.com/api/v1";
+//add changes
 const instance = axios.create({
     baseURL,
     timeout: 10000,
