81afcf9e410e88d6416b701c66aad0dfa976106b
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-10-23 17:01:26 +0530

    push

--- src/instance/protectedInstance.js
+++ src/instance/protectedInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// const baseURL = "http://localhost:5000/api/v1";
+
 const baseURL = "https://ecommerce-backend-1-ldht.onrender.com/api/v1";
 //add chnages
 const protectedInstance = axios.create({
